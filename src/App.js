8761b365d0723a65fa26435a006bc6d10416cca3
VladZimin <vlad.zimin1@gmail.com>
2022-01-11 19:44:09 +0500

    get pizzas items

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import { Header } from './components/Header'
 import { Home } from './pages/Home'
 import { Cart } from './pages/Cart'
 import { Route, Routes } from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
+  const [pizzaItems, setPizzaItems] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json')
+      .then(({ data }) => setPizzaItems(data.pizzas))
+  }, [])
+
   return (
     <div className='App'>
       <div className='wrapper'>
@@ -13,7 +21,7 @@ function App() {
         <div className='content'>
           <div className='container'>
             <Routes>
-              <Route path='/' element={<Home />} />
+              <Route path='/' element={<Home items={pizzaItems} />} />
               <Route path='/cart' element={<Cart />} />
             </Routes>
           </div>
